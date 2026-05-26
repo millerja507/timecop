3c5da0d814b5204801f4a208ebd455199eb19090
Jordan Miller <JordanA.Miller@hepi.com>
2026-05-26 16:40:02 -0500

    Bump version to v1.3.0 after recent sync improvements and colonless time entries

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ function playSound(type) {
 
 // --- App State & Data Structures ---
 let state = {
-  version: '1.2.0',
+  version: '1.3.0',
   projects: [],
   punches: [],
   activePunchId: null,
